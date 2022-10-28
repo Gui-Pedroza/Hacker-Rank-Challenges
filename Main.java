bd2cd5315cd621a9fb13d69a55bdfe68e2bab320
Gui-Pedroza <109224884+Gui-Pedroza@users.noreply.github.com>
2022-10-28 13:53:28 -0300

    Commented Main.java
    The Calendar class is an abstract class that provides methods for converting between a specific instant in time and a set of calendar fields such as YEAR, MONTH, DAY_OF_MONTH, HOUR, and so on, and for manipulating the calendar fields, such as getting the date of the next week.
    
    You are given a date. You just need to write the method, getDay() , which returns the day on that date

--- Main.java
+++ Main.java
@@ -8,13 +8,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println(findDay(12, 25, 2059));
-
+		System.out.println(findDay(int day, int month, int year));
+		//return the day of week, in capital letters, given a date
+		// 2000 < year < 3000
 	}
 
 	public static String findDay(int month, int day, int year) {
 		Calendar c = Calendar.getInstance();
-		c.set(year, month-1, day);
+		c.set(year, month-1, day); //month starts with 0-January
 		int dayNumber = c.get(Calendar.DAY_OF_WEEK);		
 		switch (dayNumber) {
 		case 1:
